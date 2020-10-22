1fc9f2066b82d6c40f9a63cd3b964c44464ea0cc
Ronald <kyoberonald@gmail.com>
2020-10-22 15:17:23 +0300

    props, children and detructuring

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -38,14 +38,14 @@ function BookList(){
 }
 
 
-const Book = ({img, title, author, children}) =>{
-  // const {img, title, author} = props;
+const Book = ({props}) =>{
+  const {img, title, author} = props;
   return (
    <article className='book'>
      <img src={img} alt='' />
     <h1>{title}</h1>
     <h4>{author}</h4>
-    {children}
+    {props.children}
   </article>
   );
 };
