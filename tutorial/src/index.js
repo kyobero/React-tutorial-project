aec34090573f9750ca6a3720c4eee73ba602f1d6
Ronald <kyoberonald@gmail.com>
2020-10-22 14:59:46 +0300

    Destructuring method two

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -35,8 +35,8 @@ function BookList(){
 }
 
 
-const Book = (props) =>{
-  const {img, title, author} = props;
+const Book = ({img, title, author}) =>{
+  // const {img, title, author} = props;
   return (
    <article className='book'>
      <img src={img} alt='' />
