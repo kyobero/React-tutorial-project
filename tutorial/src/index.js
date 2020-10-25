5ef188bc0ffa1dea548362b436b2efe0a421e526
Ronald <kyoberonald@gmail.com>
2020-10-25 15:01:49 +0300

    Detructuring+looping through the object using the spread operator

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -6,18 +6,21 @@ import './index.css';
 // setup vars
 const books = [
 {
+  id: 1,
   img: 
   'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
   title: 'I love you to the moon and back',
   author: 'Amelia Hepworth',
 },
 {
+  id: 2,
   img: 
   'https://images-na.ssl-images-amazon.com/images/I/81zBVMvSjNL._AC_UL200_SR200,200_.jpg',
   title: 'Midnight Sun',
   author: 'Stephenie Meyer',
 },
 {
+  id: 3,
   img: 
   'https://images-na.ssl-images-amazon.com/images/I/81zBVMvSjNL._AC_UL200_SR200,200_.jpg',
   title: 'Midnight Sun',
@@ -29,8 +32,8 @@ const books = [
 function BookList() {
   return (
   <section className='booklist'>
-    {books.map((book)=> {
-      return  <Book book={book}></Book>;
+    {books.map((book, index)=> {
+      return  <Book key={book.id} {...book}></Book>;
     
   })}
   </section>
@@ -38,15 +41,14 @@ function BookList() {
 }
 
 
-const Book = (props) =>{
-  const {img, title, author} = props.book;
-
+const Book = ({img, title, author}) =>{
+  // const {img, title, author} = props;
+  // console.log(props);
   return (
    <article className='book'>
      <img src={img} alt='' />
     <h1>{title}</h1>
     <h4>{author}</h4>
-    <h3>{props.children}</h3> 
   </article>
   );
 };
