e101e8a19ba75b9f75c80f05c4d11b74a1688fe4
Ronald <kyoberonald@gmail.com>
2020-10-25 16:54:54 +0300

    onClick and on mouseOver methods+Event baics

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -45,15 +45,27 @@ const Book = ({img, title, author}) =>{
   // const {img, title, author} = props;
   // attribute, eventHandler
   // onClick, onMoueOver
-  const clickHandler = () => {
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
     alert('hello world');
   };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-   <article className='book'>
+   <article className='book' onMouseOver={()=>{
+     console.log(title);
+   }}>
      <img src={img} alt='' />
     <h1 onClick={() => console.log(title)}>{title}</h1>
     <h4>{author}</h4>
-    <button type='button' onClick={clickHandler}>reference example</button>
+    <button type='button' onClick={clickHandler}>
+     reference example
+    </button>
+    <button type='button' onClick={() => complexExample(author)}>
+      more complex example
+    </button>
   </article>
   );
 };
